be84f460a8303e34939647e6e5329c0f6c90f0a9
MAERTENS Yann-Christophe <iroshinidako@gmail.com>
2023-11-21 12:00:40 +0100

    Initial Commit

---
+++ ChezTotoProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ChezTotoProject
+//
+//  Created by Yann Christophe Maertens on 21/11/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
